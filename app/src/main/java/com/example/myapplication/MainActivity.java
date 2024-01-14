2c683f9a98b42f0d523e961b11044baa10feccf3
Sinh1705 <sinhteogp@gmail.com>
2024-01-15 03:12:21 +0700

    this 15

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    String test="day la du lieu moi";
     RecyclerView recyclerView;
     PhimAdapter phimAdapter;
 
