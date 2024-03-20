9154370e6e558912941375a2b6076a68ba0874c8
nguyenvu232901 <nguyentuanvu2002hn@gmail.com>
2024-03-20 09:51:57 +0700

    20/3

--- app/src/main/java/com/example/myapplication/user/Details_Category.java
+++ app/src/main/java/com/example/myapplication/user/Details_Category.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -12,18 +11,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.Phim;
 import com.example.myapplication.R;
-import com.example.myapplication.TheLoai;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class Details_Category extends AppCompatActivity {
     private RecyclerView recyclerView;
     private PhimUserAdapter phimUserAdapter;
-    String tentheloai = "";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,27 +28,9 @@ public class Details_Category extends AppCompatActivity {
 
         String categoryId = getIntent().getStringExtra("category_id");
 
-        FirebaseDatabase.getInstance().getReference().child("category").child(categoryId)
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if(snapshot.exists()){
-                            TheLoai theLoai = snapshot.getValue(TheLoai.class);
-                            if(theLoai!=null){
-                                tentheloai = theLoai.getTheloai();
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        // Xử lý khi có lỗi
-                    }
-                });
-
         //truy van ds phim thuoc the loai va hien thi
         FirebaseRecyclerOptions<Phim> options = new FirebaseRecyclerOptions.Builder<Phim>()
-                .setQuery(FirebaseDatabase.getInstance().getReference().child("phim").child(tentheloai), Phim.class)
+                .setQuery(FirebaseDatabase.getInstance().getReference().child("phim").child(categoryId), Phim.class)
                 .build();
         phimUserAdapter = new PhimUserAdapter(options, this);
         recyclerView.setAdapter(phimUserAdapter);
